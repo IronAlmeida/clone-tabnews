1c5aad1abaa1c2e037d6cb69fbd026f421306f3c
Iron Almeida <54252720+IronAlmeida@users.noreply.github.com>
2023-08-19 16:08:23 +0000

    adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home (){
-    return (
-        <h1>Hello World! Projeto para evoulir na programação.</h1>
-    )
+  return (
+    <h1>Hello World! Projeto para evoulir na programação.</h1>
+  )
 }
  
 export default Home;
