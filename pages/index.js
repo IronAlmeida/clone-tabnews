7196cd48d3098b8d03d0aecaf7823a9a229c7ef4
Iron Almeida <54252720+IronAlmeida@users.noreply.github.com>
2023-08-19 16:57:21 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-function Home (){
-  return (
-    <h1>Hello World! Projeto para evoulir na programação.</h1>
-  )
+function Home() {
+  return <h1>Hello World! Projeto para evoulir na programação.</h1>;
 }
- 
+
 export default Home;
